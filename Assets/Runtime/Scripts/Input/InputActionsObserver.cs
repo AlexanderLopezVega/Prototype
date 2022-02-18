e2cfedad566f4d3d2a3be97c8e9783be68ac105d
Alex Lopez <alex.lopez.ve@gmail.com>
2022-02-18 09:25:39 +0100

    Changed to new input system

---
+++ Assets/Runtime/Scripts/Input/InputActionsObserver.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace com.alexlopezvega.prototype
+{
+    public class InputActionsObserver : MonoBehaviour
+    {
+        // Constant Fields
+
+        // Fields
+
+        // Constructors
+
+        // Finalizers (Destructors)
+
+        // Delegates
+
+        // Events
+
+        // Enums
+
+        // Interfaces (interface implementations)
+
+        // Properties
+
+        // Indexers
+
+        // Methods
+        
+        // Structs
+
+        // Classes
+
+    }
+}
