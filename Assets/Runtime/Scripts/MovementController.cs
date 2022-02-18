1e59aeed20be0029f6c9988057ba55ccb3142aa8
Alex Lopez <alex.lopez.ve@gmail.com>
2022-02-18 09:15:57 +0100

    Started working on omniball
    All-direction rotation ball for player controller

---
+++ Assets/Runtime/Scripts/MovementController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace com.alexlopezvega.prototype
+{
+    public class MovementController : MonoBehaviour
+    {
+        // Constant Fields
+
+        // Fields
+
+        // Constructors
+
+        // Finalizers (Destructors)
+
+        // Delegates
+
+        // Events
+
+        // Enums
+
+        // Interfaces (interface implementations)
+
+        // Properties
+
+        // Indexers
+
+        // Methods
+
+        // Structs
+
+        // Classes
+
+    }
+}
