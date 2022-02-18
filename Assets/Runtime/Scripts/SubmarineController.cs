a176d40b0eca974ad00556bb47c68eafe679d172
Alex Lopez <alex.lopez.ve@gmail.com>
2022-02-18 12:56:17 +0100

    Started working on buoyancy

--- Assets/Runtime/Scripts/SubmarineController.cs
+++ Assets/Runtime/Scripts/SubmarineController.cs
@@ -6,14 +6,23 @@ namespace com.alexlopezvega.prototype
     public class SubmarineController : MonoBehaviour
     {
         // Constant Fields
+        private const float WaterDensity = 1000f;
 
         // Fields
         [Header("Depdendencies")]
         [SerializeField] private Rigidbody submarineRigidbody = default;
+        [Header("Data")]
+        [SerializeField, Min(0f)] private float totalNonHullVolume = default;
+        [SerializeField, Min(0f)] private float totalHullEmptyVolume = default;
+        [SerializeField, Min(0f)] private float fillSpeed = 1f;
 
+        // Input
         private float throttleInput = default;
         private float steerInput = default;
         private float ascentInput = default;
+
+        // Physics
+        private float fillAmount = 0.5f;
 
         // Constructors
 
@@ -42,13 +51,30 @@ namespace com.alexlopezvega.prototype
         }
         private void OnDestroy()
         {
-            if(AssetFinder.TryFindComponent(TagCts.InputActionsObserver, out InputActionsObserver inputActionsObserver))
+            if (AssetFinder.TryFindComponent(TagCts.InputActionsObserver, out InputActionsObserver inputActionsObserver))
             {
                 inputActionsObserver.Vehicle.OnThrottleActionEvent -= OnThrottleAction;
                 inputActionsObserver.Vehicle.OnSteerActionEvent -= OnSteerAction;
                 inputActionsObserver.Vehicle.OnAscentActionEvent -= OnAscentAction;
             }
         }
+
+        private void Update()
+        {
+            float dt = Time.deltaTime;
+
+            fillAmount = Mathf.Clamp01(Mathf.MoveTowards(fillAmount, fillAmount + ascentInput, (fillSpeed / totalHullEmptyVolume) * dt));
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 nonHullForce = CalculateBuoyantForce(totalNonHullVolume);
+            Vector3 hullForce = CalculateBuoyantForce((1f - fillAmount) * totalHullEmptyVolume);
+
+            submarineRigidbody.AddForce(hullForce + nonHullForce, ForceMode.Force);
+        }
+
+        private static Vector3 CalculateBuoyantForce(float volume) => WaterDensity * volume * -Physics.gravity;
 
         private void OnThrottleAction(CallbackContext context) => throttleInput = context.ReadValue<float>();
         private void OnSteerAction(CallbackContext context) => steerInput = context.ReadValue<float>();
