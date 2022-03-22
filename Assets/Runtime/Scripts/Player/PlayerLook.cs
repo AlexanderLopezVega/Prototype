35ee3bf0fe1d08839983cdd1317e0166b1debc10
Alex Lopez <alex.lopez.ve@gmail.com>
2022-03-22 15:23:32 +0100

    Unknown changes

---
+++ Assets/Runtime/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace com.alexlopezvega.prototype
+{
+    public class PlayerLook : MonoBehaviour
+    {
+        [Header("Dependencies")]
+        [SerializeField] private Transform playerRoot = default;
+        [SerializeField] private Transform cameraRoot = default;
+
+        private bool shouldUpdateForward = default;
+
+        private void Start()
+        {
+            AssetFinder.FindComponent<InputActionsObserver>(TagCts.InputActionsObserver).Player.OnMoveActionEvent += OnMoveAction;
+        }
+        private void OnDestroy()
+        {
+            if (!AssetFinder.TryFindComponent(TagCts.InputActionsObserver, out InputActionsObserver iao))
+                return;
+
+            iao.Player.OnMoveActionEvent -= OnMoveAction; 
+        }
+
+        private void Update()
+        {
+            if (shouldUpdateForward)
+                UpdateForward();
+        }
+
+        private void UpdateForward()
+        {
+            Vector3 projCamToPlayer = Vector3.ProjectOnPlane(playerRoot.position - cameraRoot.position, Vector3.up).normalized;
+
+            playerRoot.rotation = Quaternion.LookRotation(projCamToPlayer, Vector3.up);
+        }
+
+        private void OnMoveAction(CallbackContext ctx)
+        {
+            if (ctx.performed)
+                shouldUpdateForward = true;
+            else if(ctx.canceled)
+                shouldUpdateForward = false;
+        }
+    }
+}
